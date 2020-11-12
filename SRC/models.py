de5d6296ec0edfd371a09d35375a739c0a2734f9
Kevin <kevingtxz@gmail.com>
2020-11-12 20:27:09 -0300

    Models

--- SRC/models.py
+++ SRC/models.py
@@ -1,3 +1,157 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class State(models.Model):
+    initials = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.initials
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    state = models.ForeignKey(State, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+class Region(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+
+class Address(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    neighborhood = models.CharField(max_length=200)
+    number = models.CharField(max_length=20, blank=True, null=True)
+    complement = models.CharField(max_length=200, blank=True, null=True)
+    
+    region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True)
+    city = models.ForeignKey(City, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.neighborhood
+
+EDUCATIONS = [
+    ('M', 'Master'),
+    ('H', 'High School'),
+    ('P', 'Phd'),
+    ('D', 'Degree'),
+    ('T', 'Technical'),
+    ('F', 'Fundamental'),
+]
+
+class Mentor(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    profile_pic = models.ImageField(default='profile1.png', blank=True, null=True)
+    nickname = models.CharField(max_length=200, blank=True, null=True)
+    firstname = models.CharField(max_length=100, blank=True, null=True)
+    lastname = models.CharField(max_length=200, blank=True, null=True)
+    birth = models.CharField(max_length=8, blank=True, null=True)
+    phone = models.CharField(max_length=11, blank=True, null=True)
+    education_Level = models.CharField(max_length=1, choices=EDUCATIONS)
+    education = models.CharField(max_length=200, blank=True, null=True)
+    telegram = models.CharField(max_length=200, blank=True, null=True)
+    mastery_area = models.CharField(max_length=200, blank=True, null=True)
+    about_me = models.CharField(max_length=5000, blank=True, null=True)
+    linkedin = models.CharField(max_length=200, blank=True, null=True)
+    latters	= models.CharField(max_length=200, blank=True, null=True)
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE)
+    carrear = models.OneToOneField('Carrear', on_delete=models.CASCADE)
+
+
+
+class DaysUsing(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+class Carrear(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    days_working = models.ForeignKey(DaysUsing, on_delete=models.CASCADE)
+    position_region_ranking = models.IntegerField(blank=True)
+    position_ranking = models.IntegerField(blank=True)
+    points_general = models.IntegerField(blank=True)
+
+
+class Page(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+class Content(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    title = models.CharField(max_length=200, blank=True, null=True)
+    pages = models.ManyToManyField(Page)
+
+class Elo(models.Model):
+    mentors = models.ManyToManyField(Mentor)
+    contents = models.ManyToManyField(Content)
+    class Meta:
+        abstract = True
+
+class Bronze(Elo):
+    name = models.CharField(max_length=6, default='Bronze')
+
+class Silver(Elo):
+    name = models.CharField(max_length=6, default='Silver')
+
+class Gold(Elo):
+    name = models.CharField(max_length=4, default='Gold')
+
+TYPEPERMISSIONS = [
+    ('P', 'POSTER'),
+]
+
+
+class UserPermission(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    mentor = models.OneToOneField(Mentor, on_delete=models.CASCADE)
+    type_permission = models.CharField(max_length=1)
+
+class UserPermissionCommentary(UserPermission):
+    commentary = models.ForeignKey('Commentary', on_delete=models.CASCADE)
+
+class Commentary(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    text = models.CharField(max_length=500)
+    is_public = models.BooleanField(default=False)
+    mentor = models.ForeignKey(Mentor, on_delete=models.CASCADE)
+
+class Vote(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    mentor = models.ForeignKey(Mentor, on_delete=models.CASCADE)
+    user_permission_poster = models.ForeignKey(UserPermission, on_delete=models.CASCADE)
+    class Meta:
+        abstract = True
+
+class CharismVote(Vote):
+    pass
+
+class EngagementVote(Vote):
+    pass
+
+class PerseveranceVote(Vote):
+    pass
+
+class EthicsVote(Vote):
+    pass
+
+class BusinessStrategyVote(Vote):
+    pass
+
+class SalesVote(Vote):
+    pass
+
+class CommunicationVote(Vote):
+    pass
+
+class NetworkingVote(Vote):
+    pass
+
+class KnowledgeVote(Vote):
+    pass
+
+class FocusVote(Vote):
+    pass
